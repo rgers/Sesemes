8c227b8c21a5543f8b1ef11b8264c3d8a1c54fee
Radek <madwolf@o2.pl>
2011-02-25 00:12:55 +0100

    first commit

---
+++ src/pl/gers/sesemes/Sesemes.java
@@ -0,0 +1,358 @@
+package pl.gers.sesemes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.params.HttpClientParams;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.impl.client.AbstractHttpClient;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
+import org.apache.http.message.BasicHttpRequest;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
+import pl.gers.sesemes.R;
+import android.R.string;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.net.http.AndroidHttpClient;
+import android.os.Bundle;
+import android.provider.Contacts;
+import android.provider.Contacts.People;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Sesemes extends Activity implements OnClickListener{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button btn_wyslij = (Button) findViewById(R.id.btn_wyslij);
+		btn_wyslij.setOnClickListener(this);
+		Button btn_contact = (Button) findViewById(R.id.btn_contact);
+		btn_contact.setOnClickListener(this);
+    }
+
+	@Override
+	public void onClick(View v) 
+	{
+		int id = v.getId();
+		switch(id)
+		{
+		case (R.id.btn_wyslij):
+			send_sms();
+		break;
+		
+		case (R.id.btn_contact):
+			get_contact();
+		break;
+		
+		}
+		
+	}
+	public void get_contact()
+	{
+	Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds. Phone.CONTENT_URI);
+	startActivityForResult(i, 1);
+	}
+	  @Override  
+	    public void onActivityResult(int reqCode, int resultCode, Intent data) {  
+	        super.onActivityResult(reqCode, resultCode, data);  
+	  
+	        switch (reqCode) {  
+	            case (1):  
+	                if (resultCode == Activity.RESULT_OK) {  
+	                    Uri contactData = data.getData();  
+	                    Cursor c = managedQuery(contactData, null, null, null, null);  
+	                    if (c.moveToFirst()) {  
+	                        String name = c.getString(c.getColumnIndexOrThrow(ContactsContract.CommonDataKinds. Phone.NUMBER));  
+	                        EditText txt_numer = (EditText) findViewById(R.id.txt_numer);
+	                        txt_numer.setText(name);  
+	                    }  
+	                }  
+	                break;  
+	        }  
+	    }  
+
+	public void send_sms()
+	{
+		 HttpResponse resp = null;
+		 SchemeRegistry schemeRegistry = new SchemeRegistry();
+		 schemeRegistry.register(new Scheme("https", 
+		             SSLSocketFactory.getSocketFactory(), 443));
+		 HttpParams params = new BasicHttpParams();
+		 HttpContext localContext = null;
+		 SingleClientConnManager mgr = new SingleClientConnManager(params, schemeRegistry);
+		HttpClient klient = new DefaultHttpClient();
+		CookieStore cookieJar = new BasicCookieStore();
+		EditText txt_wiadomosc = (EditText) findViewById(R.id.txt_wiadomosc);
+		EditText txt_numer = (EditText) findViewById(R.id.txt_numer);
+		// Creating a local HTTP context
+		localContext = new BasicHttpContext();
+
+		// Bind custom cookie store to the local context
+		localContext.setAttribute(ClientContext.COOKIE_STORE, cookieJar);
+		HttpUriRequest urirequest;
+		HttpPost postr = new HttpPost();
+		try {
+			postr.setURI(new URI("https://www.orange.pl/start.phtml"));
+		} catch (URISyntaxException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		try {
+			resp = klient.execute(postr, localContext);
+		
+			int a=0;
+			while(a<cookieJar.getCookies().size())
+			{
+			Log.v("cookie", cookieJar.getCookies().get(a).getName() + "=" + cookieJar.getCookies().get(a).getValue());
+			a++;
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		try {
+			postr.setURI(new URI("https://www.orange.pl/start.phtml?_DARGS=/gear/infoportal/header/user-box.jsp"));
+		} catch (URISyntaxException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		//HttpHost host = new HttpHost("http://www.orange.pl/start.phtml?_DARGS=/gear/infoportal/header/user-box.jsp");
+		HttpHost host = new HttpHost("www.google.pl");
+		
+
+		params.setParameter("_dyncharset", "UTF-8");
+		params.setParameter("/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.userLogin", "radekg");
+		params.setParameter("_D:/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.userLogin", " ");
+		params.setParameter("/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.userPassword", "al0nz0");
+		params.setParameter("_D:/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.userPassword", " ");
+		params.setParameter("/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.successUrl", "/start.phtml");
+		params.setParameter("_D:/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.successUrl", " ");
+		params.setParameter("/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.errorUrl", "/start.phtml");
+		params.setParameter("_D:/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.errorUrl", " ");
+		params.setParameter("/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.login", "loguj");
+		params.setParameter("_D:/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.login", " "); 
+		params.setParameter("x", "23");
+		params.setParameter("y", "5");
+		params.setParameter("_DARGS", "/gear/infoportal/header/user-box.jsp");
+		List<NameValuePair> pairs = new ArrayList<NameValuePair>(); 
+	
+		pairs.add(new BasicNameValuePair("_dyncharset", "UTF-8"));
+		pairs.add(new BasicNameValuePair("/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.userLogin", "radekg"));
+		pairs.add(new BasicNameValuePair("_D:/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.userLogin", " "));
+		pairs.add(new BasicNameValuePair("/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.userPassword", "al0nz0"));
+		pairs.add(new BasicNameValuePair("_D:/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.userPassword", " "));
+		pairs.add(new BasicNameValuePair("/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.successUrl", "/start.phtml"));
+		pairs.add(new BasicNameValuePair("_D:/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.successUrl", " "));
+		pairs.add(new BasicNameValuePair("/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.errorUrl", "/start.phtml"));
+		pairs.add(new BasicNameValuePair("_D:/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.errorUrl", " "));
+		pairs.add(new BasicNameValuePair("/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.login", "loguj"));
+		pairs.add(new BasicNameValuePair("_D:/ptk/map/infoportal/portlet/header/formhandler/ProxyProfileFormhandler.login", " ")); 
+		pairs.add(new BasicNameValuePair("x", "23"));
+		pairs.add(new BasicNameValuePair("y", "5"));
+		pairs.add(new BasicNameValuePair("_DARGS", "/gear/infoportal/header/user-box.jsp"));
+		
+		try {
+		postr.setEntity(new UrlEncodedFormEntity(pairs));
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+		e1.printStackTrace();
+		} 
+		try {
+			postr.setParams(params);
+			postr.setHeader("Accept", "application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5");
+			postr.setHeader("Content-Type", "application/x-www-form-urlencoded");
+			postr.setHeader("Origin", "http://www.orange.pl");
+			postr.setHeader("Referer", "http://www.orange.pl/start.phtml");
+			postr.setHeader("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.98 Safari/534.13");
+			resp = klient.execute(postr, localContext);
+			int a=0;
+			while(a<cookieJar.getCookies().size())
+			{
+			Log.v("cookie", cookieJar.getCookies().get(a).getName() + "=" + cookieJar.getCookies().get(a).getValue());
+			a++;
+			}
+		/*	HttpPost postr2 = new HttpPost();
+			try {
+				postr2.setURI(new URI("http://www.orange.pl/portal/map/map/message_box?mbox_edit=new&stamp=1298549505941&mbox_view=newsms"));
+			} catch (URISyntaxException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+		//	resp = klient.execute(postr2);
+		*/} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		HttpPost postr2 = new HttpPost();
+		HttpGet getr = new HttpGet();
+		try {
+			
+			getr.setURI(new URI("http://www.orange.pl/portal/map/map/corpo_message_box?mbox_edit=new&stamp=1298579037096&mbox_view=newsms"));
+			
+		} catch (URISyntaxException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} 
+		
+		try {
+			resp = klient.execute(getr, localContext);
+			
+		} catch (ClientProtocolException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		BufferedReader in=null;
+		 try {
+			in = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+         StringBuffer sb = new StringBuffer("");
+         String line = "";
+         String NL = System.getProperty("line.separator");
+         try {
+			while ((line = in.readLine()) != null) {
+			     sb.append(line + NL);
+			 }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+         try {
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int token_indx = sb.indexOf("MessageFormHandler.token");
+		token_indx=token_indx-74;
+		String smstoken = sb.substring(token_indx, token_indx+15);
+		Log.v("cookie",smstoken);
+		List<NameValuePair> pairs1 = new ArrayList<NameValuePair>(); 
+		
+		pairs1.add(new BasicNameValuePair("_dyncharset", "UTF-8"));
+		pairs1.add(new BasicNameValuePair("/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.type", "sms"));
+		pairs1.add(new BasicNameValuePair("_D:/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.type", " "));
+		pairs1.add(new BasicNameValuePair("enabled", "false"));
+		pairs1.add(new BasicNameValuePair("/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.errorURL", "/portal/map/map/message_box?mbox_view=newsms"));
+		pairs1.add(new BasicNameValuePair("_D:/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.errorURL", " "));
+		pairs1.add(new BasicNameValuePair("/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.successURL", "/portal/map/map/message_box?mbox_view=messageslist"));
+		pairs1.add(new BasicNameValuePair("_D:/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.successURL", " "));
+		pairs1.add(new BasicNameValuePair("/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.to", txt_numer.getText().toString()));
+		pairs1.add(new BasicNameValuePair("_D:/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.to", " "));
+		pairs1.add(new BasicNameValuePair("_D:/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.body", " "));
+		pairs1.add(new BasicNameValuePair("/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.body", txt_wiadomosc.getText().toString()));
+		pairs1.add(new BasicNameValuePair("/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.token", smstoken));
+		pairs1.add(new BasicNameValuePair("_D:/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.token", " "));
+		pairs1.add(new BasicNameValuePair("/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.create.x", "51"));
+		pairs1.add(new BasicNameValuePair("/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.create.y", "9"));
+		pairs1.add(new BasicNameValuePair("/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.create", "Wyœlij"));
+		pairs1.add(new BasicNameValuePair("_D:/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.create", " "));
+		pairs1.add(new BasicNameValuePair("_DARGS", "/gear/mapmessagebox/smsform.jsp"));	
+		try {
+		postr.setEntity(new UrlEncodedFormEntity(pairs1));
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+		e1.printStackTrace();
+		} 
+		try {
+			postr.setURI(new URI("http://www.orange.pl/portal/map/map/message_box?_DARGS=/gear/mapmessagebox/smsform.jsp"));
+		} catch (URISyntaxException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		postr.setHeader("Accept", "application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5");
+		postr.setHeader("Content-Type", "application/x-www-form-urlencoded");
+		postr.setHeader("Origin", "http://www.orange.pl");
+		postr.setHeader("Referer", "http://www.orange.pl/portal/map/map/message_box?mbox_edit=new&stamp=1298580685001&mbox_view=newsms");
+		postr.setHeader("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.98 Safari/534.13");
+		try {
+			resp = klient.execute(postr, localContext);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 try {
+				in = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	         sb = new StringBuffer("");
+	         line = "";
+	         NL = System.getProperty("line.separator");
+	         try {
+				while ((line = in.readLine()) != null) {
+				     sb.append(line + NL);
+				 }
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	         try {
+				in.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		int a=0;
+		while(a+50<sb.length())
+		{
+         String page = sb.substring(a,a+50);
+         Log.v("html", page);
+         a=a+50;
+		}
+	}
+}
